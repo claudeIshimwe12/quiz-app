f2987128a24ff09f5ff373f883026684f5ffc682
Claude Ishimwe <claudeishimwe@amalitechtraining.org>
2024-07-04 09:55:17 +0200

    add favicon and nav-bar component

---
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+// import iconSunLight from '../../../assets/images/icon-sun-light.svg';
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.css',
+})
+export class NavBarComponent {
+  // iconSunLight = iconSunLight;
+}
