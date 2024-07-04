e73a3e5ac704337b333228a0ff0aa508e15e5055
Claude Ishimwe <claudeishimwe@amalitechtraining.org>
2024-07-04 10:46:06 +0200

    add background images and custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        puple: "#A729F5",
+        "dark-navy": "#3B4D66",
+        navy: "#3B4D66",
+        "grey-navy": "#626C7F",
+        "light-bluish": "#ABC1E1",
+        "light-greyish": "#F4F6FA",
+        white: "#FFFFFF",
+        green: "#26D782",
+        red: "#EE5454",
+      },
+    },
   },
   plugins: [],
 };
